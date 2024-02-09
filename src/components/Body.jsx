d2a9ef2355dc382922c865c4052d80669608037d
pavankalvium1 <pavanew11@gmail.com>
2024-02-09 10:20:09 +0530

    updates

---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+// Body.jsx
+import React from 'react';
+
+const Body = ({ imageData }) => {
+  return (
+    <div>
+      {
+        imageData.map(image => (
+          <div key={image.id} className="column">
+            <img src={image.img} alt="" />
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+export default Body;
